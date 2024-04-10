48eeef70e6b7451c1485a3a01dcae2561c45f429
Ivan Dmitirev <vanya2003569@yandex.ru>
2024-04-10 19:30:21 +0300

    Тренировка на C#

---
+++ Practika2/ModelWindow.cs
@@ -0,0 +1,135 @@
+﻿using System.Drawing;
+
+namespace Practika2
+{
+    internal class Screen
+    {
+        static public int HorizontalSize { get; private set; } = 30;
+        static public int VerticalSize { get; private set; } = 30;
+        private char[,] _screenArray = new char[VerticalSize, HorizontalSize];
+        private ModelWindow _window;
+
+        public Screen(ModelWindow window)
+        {
+            _window = window;
+            Fill();
+        }
+        public Screen() : this(new ModelWindow()) { }
+
+        public void PrintScreen()
+        {
+            if (_window == null || (_window.HorizontalSize == 0 || _window.VerticalSize == 0)) return;
+            Console.WriteLine($"{_window.NameWindow}");
+            PrintWindow();
+            for (int i = 0; i < VerticalSize; ++i)
+            {
+                for (int j = 0; j < HorizontalSize; ++j)
+                {
+                    Console.Write(_screenArray[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("→ - передвинуть вправо; ");
+            Console.Write("← - передвинуть влево; ");
+            Console.Write("↑ - передвинуть вверх; ");
+            Console.Write("↓ - передвинуть вниз; ");
+            Console.Write("esc - Выйти;");
+            Console.WriteLine();
+        }
+        public void Offset(ConsoleKey key)
+        {
+            switch (key)
+            {
+                case ConsoleKey.LeftArrow:
+                    _window.TopLeft = new Point(_window.TopLeft.X - 1, _window.TopLeft.Y);
+                    break;
+                case ConsoleKey.RightArrow:
+                    _window.TopLeft = new Point(_window.TopLeft.X + 1, _window.TopLeft.Y);
+                    break;
+                case ConsoleKey.UpArrow:
+                    _window.TopLeft = new Point(_window.TopLeft.X, _window.TopLeft.Y - 1);
+                    break;
+                case ConsoleKey.DownArrow:
+                    _window.TopLeft = new Point(_window.TopLeft.X, _window.TopLeft.Y + 1);
+                    break;
+            }
+            Fill();
+        }
+        private void Fill()
+        {
+            for (int i = 0; i < VerticalSize; ++i)
+                for (int j = 0; j < HorizontalSize; ++j)
+                    _screenArray[i, j] = ' ';
+        }
+        private void PrintWindow()
+        {
+            for (int i = 0; i < _window.HorizontalSize; ++i)
+            {
+                _screenArray[_window.TopLeft.Y, _window.TopLeft.X + i] = _window.SymbolWindow;
+                _screenArray[_window.TopLeft.Y + _window.VerticalSize - 1, _window.TopLeft.X + i] = _window.SymbolWindow;
+            }
+            for (int i = 0; i < _window.VerticalSize; ++i)
+            {
+                _screenArray[_window.TopLeft.Y + i, _window.TopLeft.X] = _window.SymbolWindow;
+                _screenArray[_window.TopLeft.Y + i, _window.TopLeft.X + _window.HorizontalSize - 1] = _window.SymbolWindow;
+            }
+            Console.ForegroundColor = _window.ColorWindow;
+        }
+    }
+    internal class ModelWindow
+    {
+        public string NameWindow { get; set; }
+        public ConsoleColor ColorWindow { get; set; }
+        public char SymbolWindow { get; set; }
+        private Point _topLeft;
+        public Point TopLeft
+        {
+            get { return _topLeft;}
+            set
+            {
+                if (value.X < 0 || value.Y < 0 || (value.X + HorizontalSize) > Screen.HorizontalSize || (value.Y + VerticalSize) > Screen.VerticalSize)
+                    Console.WriteLine("Невозможное присваивание. Координаты не могут быть отрицательнвми.");
+                else
+                    _topLeft = value;
+            }
+        }
+
+        private int _horizontalSize;
+        public int HorizontalSize
+        {
+            get { return _horizontalSize; }
+            set
+            {
+                if (value < 0 || (TopLeft.X + value > Screen.HorizontalSize))
+                    Console.WriteLine("Не допустимая длина окна. Превышает размеры экрана.");
+                else
+                    _horizontalSize = value;
+            }
+        }
+        private int _verticalSize;
+        public int VerticalSize
+        {
+            get { return _verticalSize; }
+            set
+            {
+                if (value < 0 || (TopLeft.Y + value) > Screen.VerticalSize)
+                    Console.WriteLine("Не допустимая длина окна. Превышает размеры экрана.");
+                else
+                    _verticalSize = value;
+            }
+        }
+
+        public ModelWindow(string nameWindow, char symbolWindow, ConsoleColor colorWindow, Point topLeft, int horizontalSize, int verticalSize) 
+        {
+            NameWindow = nameWindow;
+            SymbolWindow = symbolWindow;
+            ColorWindow = colorWindow;
+            TopLeft= topLeft;
+            HorizontalSize = horizontalSize;
+            VerticalSize = verticalSize;
+        }
+        public ModelWindow() : this("", '*', ConsoleColor.White, Point.Empty, 0, 0) { }
+
+    }
+}
---
+++ Practika2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Drawing;
+
+
+namespace Practika2
+{
+    
+    internal class Program
+    {
+        /// <summary>
+        /// Основная функция, запускающая окно
+        /// 
+        /// </summary>
+        /// <param name="nameWindow">
+        /// Имя окна
+        /// </param>
+        /// <param name="symbol">
+        /// Символ, которым будет отображаться окно
+        /// </param>
+        /// <param name="colorWindow">
+        /// Цвет окна
+        /// </param>
+        /// <param name="topLeft">
+        /// Стартовое положение окна
+        /// </param>
+        /// <param name="horizontalSizeWindow">
+        /// Длина окна
+        /// </param>
+        /// <param name="verticalSizeWindow">
+        /// Ширина окна
+        /// </param>
+        static void WindowStart(string nameWindow, char symbol, ConsoleColor colorWindow, 
+            Point topLeft, int horizontalSizeWindow, int verticalSizeWindow)
+        {
+            Screen screen = new Screen(new ModelWindow(nameWindow, symbol, colorWindow, topLeft, horizontalSizeWindow, verticalSizeWindow));
+
+            while (true)
+            {
+                screen.PrintScreen();
+                ConsoleKey key = Console.ReadKey().Key;
+                if (key == ConsoleKey.Escape) break;
+                screen.Offset(key);
+                Console.Clear();
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            WindowStart("Testing", '*', ConsoleColor.Red, new Point(0, 0), 5, 5);
+        }
+    }
+} 
